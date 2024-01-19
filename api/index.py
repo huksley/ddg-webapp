b75ecc7cc2582f82080af9f3a1dd44b9c58d7a76
Ruslan Gainutdinov <contact@ruslan.org>
2024-01-19 15:50:19 +0200

    fix: update dependencies

--- api/index.py
+++ api/index.py
@@ -1,6 +1,8 @@
 import re
-from flask import Flask, request, jsonify
-from duckduckgo_search import ddg
+#import json
+import simplejson as json
+from flask import Flask, request, jsonify, Response
+from duckduckgo_search import DDGS
 from newspaper import Article
 from flask_cors import CORS
 
@@ -9,16 +11,10 @@ CORS(app, resources={r"/*": {"origins": "https://chat.openai.com"}})
 
 @app.route('/')
 def home():
-    return 'Wow, it works!'
+    return '/search?q=your+query+here'
 
 @app.route('/search')
 def search():
-
-    # deny access to the API from non-OpenAI domains.
-    # if request.headers.get('Origin').startswith('https://chat.openai.com'):
-    # if not request.referrer or not request.referrer.startswith('https://chat.openai.com'):
-    #     return 'Access denied.', 403
-
     q = request.args.get('q')
     if not q:
         return error_response('Please provide a query.')
@@ -32,11 +28,15 @@ def search():
         max_results = request.args.get('max_results', 3, type=int)
         max_results = min(max_results, 10)
 
-        results = ddg(q, region=region, safesearch=safesearch, time=time, max_results=max_results)
-        response = jsonify(results)
-        return response
+        ddgs = DDGS()
+        results = ddgs.text(keywords=q, region=region, safesearch=safesearch, timelimit=time, max_results=max_results)
+        response = json.dumps(results, iterable_as_array=True)
+        resp = Response(response)
+        resp.headers['Content-Type'] = 'application/json'
+        return resp
 
     except Exception as e:
+        print(e)
         return error_response(f'Error searching: {e}')
 
 def escape_ddg_bangs(q):
@@ -79,7 +79,10 @@ def error_response(message):
         'title': ''
     }])
     
-    return response
+    resp = Response(response)
+    resp.status_code = 500
+    resp.headers['Content-Type'] = 'application/json'
+    return resp
 
 def extract_title_and_text_from_url(url: str):
     if not url.startswith('http://') and not url.startswith('https://'):
